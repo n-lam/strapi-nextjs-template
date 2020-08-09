c9683cd85388d4840fa1f0a64bd9f05fe4a68b4a
Nathan <nathan.lam@zoho.com>
2020-08-09 16:25:58 +1000

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '794f9422098cbfa0279a52d05edca171'),
+    },
+  },
+});
